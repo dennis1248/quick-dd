d84309a115fc63fa80779ba010569e78d76135a5
dennis1248 <dennistenhoove@protonmail.com>
2019-05-04 20:04:08 +0200

    finished basic functionality

---
+++ README.md
@@ -0,0 +1,14 @@
+# quick-dd 
+A simple Python program to run dd quicker. 
+
+![quick-dd example](/docs/images/example.gif)
+
+## Done 
+- :heavy_check_mark: Basic functionality
+
+## To-do list 
+- :x: Put ls output in array
+- :x: Print numbered array contents
+- :x: Pull image name from array with number
+
+- :x: The ability to manage pools with quick-dd 
---
+++ source/quick-dd.py
@@ -0,0 +1,35 @@
+import subprocess
+
+
+subprocess.run(['lsblk'])
+
+target = input('What is the target disk? ')
+print('/dev/' + target + ' is selected')
+
+
+def selectPool():
+    global pool
+    pool = input('From which pool do you wish to select an image? ')
+    if pool == 'os':
+        print('/mnt/sdb1/OS is selected')
+        print('------------------------------------------------------')
+        subprocess.run(['ls', '/mnt/sdb1/OS'])
+    elif pool == 'down':
+        print('~/Downloads is selected')
+        print('------------------------------------------------------')
+        subprocess.run(['ls', '/home/dennis/Downloads'])
+    else:
+        print(pool + ' is not a known pool')
+        selectPool()
+
+selectPool()
+
+image = input('Which image do you wish to flash? ')
+
+def ddRun():
+    if pool == 'os':
+        subprocess.run(['sudo', 'dd', 'if=/mnt/sdb1/OS/' + image, 'of=/dev/' + target])
+    elif pool == 'down':
+        subprocess.run(['sudo', 'dd', 'if=/home/$USER/Downloads/' + image, 'of=/dev/' + target])
+
+ddRun()

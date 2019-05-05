68628db5ff9b6bc2e772451aec535cb298c5dd1c
dennis1248 <dennistenhoove@protonmail.com>
2019-05-05 20:44:07 +0200

    Added linebreak to inputs and added arrow

--- source/quick-dd.py
+++ source/quick-dd.py
@@ -9,7 +9,7 @@ subprocess.run(['lsblk'])
 def selectTarget():
     global targetDisk
     global targetPath
-    targetDisk = input('What is the target disk? ')
+    targetDisk = input('What is the target disk?\n-> ')
     targetPath = Path('/dev/' + str(targetDisk))
     targetCheck()
 
@@ -17,12 +17,12 @@ def targetCheck():
     if targetPath.exists():
         print(str(targetPath) + ' is selected')
     else:
-        print('/dev/' + str(targetDisk) + ' does not exist, please specify an existing disk')
+        print('/dev/' + targetDisk + ' does not exist, please specify an existing disk')
         selectTarget()
 
 def selectPool():
     global pool
-    pool = input('From which pool do you wish to select an image? ')
+    pool = input('From which pool do you wish to select an image?\n-> ')
     if pool == 'os':
         print('/mnt/sdb1/OS is selected')
         subprocess.run(['ls', '/mnt/sdb1/OS'])
@@ -36,13 +36,13 @@ def selectPool():
 
 def imageTargetCheck():
     global image
-    image = input('Which image do you wish to flash? ')
+    image = input('Which image do you wish to flash?\n-> ')
 
 def ddRun():
     if pool == 'os':
-        subprocess.run(['sudo', 'dd', 'if=/mnt/sdb1/OS/' + str(image), 'of=' + str(targetPath)])
+        subprocess.run(['sudo', 'dd', 'if=/mnt/sdb1/OS/' + image, 'of=' + str(targetPath)])
     elif pool == 'down':
-        subprocess.run(['sudo', 'dd', 'if=' + homeFolder + '/Downloads' + str(image), 'of=' + str(targetPath)])
+        subprocess.run(['sudo', 'dd', 'if=' + homeFolder + '/Downloads' + image, 'of=' + str(targetPath)])
 
 selectTarget()
 selectPool()

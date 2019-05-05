88ca26d935998f990d8b4cd6b2018b7ce159b5b6
dennis1248 <dennistenhoove@protonmail.com>
2019-05-05 09:07:28 +0200

    Check if disk exists + code cleanup

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ A simple Python program to run dd quicker.
 
 ## Done
 - :heavy_check_mark: Basic functionality
+- :heavy_check_mark: Check if selected drive exists
 
 ## To-do list
 - :x: Put ls output in array
 - :x: Print numbered array contents
 - :x: Pull image name from array with number
 - :x: The ability to manage pools with quick-dd
-- :x: Check if selected drive exists 
--- source/quick-dd.py
+++ source/quick-dd.py
@@ -1,37 +1,50 @@
 #!/usr/bin/env python
 
 import subprocess
+from pathlib import Path
 
 
 subprocess.run(['lsblk'])
 
-target = input('What is the target disk? ')
-print('/dev/' + target + ' is selected')
+def selectTarget():
+    global targetDisk
+    global targetPath
+    targetDisk = input('What is the target disk? ')
+    targetPath = Path('/dev/' + str(targetDisk))
+    targetCheck()
 
+def targetCheck():
+    if targetPath.exists():
+        print(str(targetPath) + ' is selected')
+    else:
+        print('/dev/' + str(targetDisk) + ' does not exist, please specify an existing disk')
+        selectTarget()
 
 def selectPool():
     global pool
     pool = input('From which pool do you wish to select an image? ')
     if pool == 'os':
         print('/mnt/sdb1/OS is selected')
-        print('------------------------------------------------------')
         subprocess.run(['ls', '/mnt/sdb1/OS'])
     elif pool == 'down':
         print('~/Downloads is selected')
-        print('------------------------------------------------------')
         subprocess.run(['ls', '/home/dennis/Downloads'])
     else:
         print(pool + ' is not a known pool')
         selectPool()
 
-selectPool()
-
-image = input('Which image do you wish to flash? ')
+def imageTargetCheck():
+    global image
+    image = input('Which image do you wish to flash? ')
 
 def ddRun():
     if pool == 'os':
-        subprocess.run(['sudo', 'dd', 'if=/mnt/sdb1/OS/' + image, 'of=/dev/' + target])
+        subprocess.run(['sudo', 'dd', 'if=/mnt/sdb1/OS/' + str(image), 'of=' + str(targetPath)])
     elif pool == 'down':
-        subprocess.run(['sudo', 'dd', 'if=/home/dennis/Downloads/' + image, 'of=/dev/' + target])
+        subprocess.run(['sudo', 'dd', 'if=/home/dennis/Downloads/' + str(image), 'of=' + str(targetPath)])
 
+selectTarget()
+targetCheck()
+selectPool()
+imageTargetCheck()
 ddRun()
